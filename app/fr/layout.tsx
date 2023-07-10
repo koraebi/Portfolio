8d23a1474a055ad1bd6a37a4b77a7a9080ca21a6
Koraebi <63969373+koraebi@users.noreply.github.com>
2023-07-10 23:22:42 +0200

    Update layout.tsx

--- app/fr/layout.tsx
+++ app/fr/layout.tsx
@@ -1,16 +1,34 @@
 import '../globals.css';
 import React from 'react';
-import DefaultLayout from '@/layouts/DefaultLayout';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { Inter } from 'next/font/google';
+import { redirect } from 'next/navigation';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Koraebi',
   description: `Création de produits numériques en accord aux fondaments UX et aux principes d'ingénierie logicielle.`,
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  let translations;
+  try {
+    translations = (await import(`@/translations/fr.json`)).home;
+  } catch (error) {
+    redirect('/error');
+  }
+
   return (
-    <DefaultLayout>
-      {children}
-    </DefaultLayout>
-  )
+    <html lang="fr">
+      <body className="flex flex-col min-h-screen">
+        <Navbar translations={translations} locale="fr"/>
+        <div className={inter.className}>
+          {children}
+        </div>
+        <Footer locale="fr"/>
+      </body>
+    </html>
+  );
 }
